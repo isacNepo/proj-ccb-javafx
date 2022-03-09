3b8020b675d3a5a45820801c7aa61baa5fa63c90
aparecido nepomuceno <isacnepomuceno.np@gmail.com>
2022-03-09 18:27:46 -0300

    img e about

--- src/gui/FormController.java
+++ src/gui/FormController.java
@@ -3,13 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 public class FormController implements Initializable{
-
+	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		
 	}
 
+	@FXML
+	private void botaoCadastrarClicado(ActionEvent event) {
+		System.out.println("CADASTRADO");
+	}
+	
+	@FXML
+	private void botaoCancelarClicado(ActionEvent event) {
+		System.out.println("CANSELADO");
+	}
+	
 }
